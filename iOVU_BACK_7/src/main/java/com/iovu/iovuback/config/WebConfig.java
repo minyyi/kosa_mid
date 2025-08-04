0a6f4f883cdb2f1867ff4925f8381b453db9b572
minyyi <117286294+minyyi@users.noreply.github.com>
2025-08-04 10:26:42 +0900

    Add files via upload

---
+++ iOVU_BACK_7/src/main/java/com/iovu/iovuback/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.iovu.iovuback.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // React 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+  
+}
